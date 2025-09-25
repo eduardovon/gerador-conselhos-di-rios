b583b40921a399b1e55bad377199a0dcf9ae8a30
Eduardo Weidebach <eduardoweidebach@gmail.com>
2025-09-24 21:25:29 -0300

    criação e upload do README.md e dos demais arquivos

---
+++ script.js
@@ -0,0 +1,11 @@
+const vonselhos=[
+    "Acredite em você mesmo!",
+    "O primeiro passo é o mais importante!",
+    "A persistência realiza o impossivel!",
+    "Aprenda algo novo hoje!",
+    "Seja gentil com todos que encontar!"
+];
+
+console.log("Arquivo de script carrregado. Array de conselhos pronto.");
+  
+
